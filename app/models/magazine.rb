c67488ef4f01d1a4d85f475c38afae16e7509265
Dave Roberts <david.a.roberts@gd-ais.com>
2014-11-03 17:09:10 -0500

    Failing tests

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,7 +1,7 @@
 class Magazine < ActiveRecord::Base
+  after_create :reader_create
   has_many :readers, through: :subscriptions
   has_many :subscriptions
-  after_create :reader_create
   def reader_create
     r = Reader.new(name: 'auto_created_reader')
     self.readers << r
--- test/models/magazine_test.rb
+++ test/models/magazine_test.rb
@@ -5,9 +5,5 @@ class MagazineTest < ActiveSupport::TestCase
     m = Magazine.create(name: 'Vanity Fair')
     m.save
     assert m.subscriptions.count == 1
-    r2 = Reader.new(name: 'r2d2')
-    m.readers << r2
-    binding.pry
-    assert m.subscriptions.count == 2
   end
 end
